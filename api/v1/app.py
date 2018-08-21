47a24dc3d17197fa8a3e52825a58cbeb221bbb71
[GucciGerm] <germaine.balmania@gmail.com>
2018-08-20 19:23:03 -0700

    Adding updated app.py for just for 404 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
 
     from models import storage
     from api.v1.views import app_views
+    from flask import jsonify
     import flask
 
     app = flask.Flask(__name__)
@@ -14,5 +15,10 @@ if __name__ == "__main__":
     @app.teardown_appcontext
     def flask_teardown(error):
         storage.close()
+
+    @app.errorhandler(404)
+    def page_not_found(exception):
+        d = {"error": "Not found"}
+        return (jsonify(d))
 
     app.run(threaded=True, host="0.0.0.0", port=5000)
