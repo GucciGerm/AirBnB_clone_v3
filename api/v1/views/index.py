960936dcc4f8ffc45273c16dbf4f50740fbeb049
[GucciGerm] <germaine.balmania@gmail.com>
2018-08-20 19:09:19 -0700

    Adding updated index that displays dictionary with key, vals

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,23 @@
     Create restful API for project HBNB
 """
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
 
 @app_views.route("/status")
 def status():
     d = {"status": "OK"}
+    return (jsonify(d))
+
+@app_views.route("/stats")
+def count_up():
+    d = {}
+
+    d["amenities"] = storage.count("Amenity")
+    d["cities"] = storage.count("City")
+    d["places"] = storage.count("Place")
+    d["reviews"] = storage.count("Review")
+    d["states"] = storage.count("State")
+    d["users"] = storage.count("User")
+
     return (jsonify(d))
