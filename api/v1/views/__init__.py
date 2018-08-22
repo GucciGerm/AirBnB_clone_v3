a3b4336b9a323436dd997dee8cccaf03730b9d9a
[GucciGerm] <germaine.balmania@gmail.com>
2018-08-22 16:01:50 -0700

    Adding updated init file with underscore __name__

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,6 @@
     Create restful API for hbnb project
 """
 from flask import Blueprint
-app_views = Blueprint("app_views", __name_, url_prefix="/api/v1")
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 #from api.v1.views.states import *
