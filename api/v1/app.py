671c20949d83c8a317374fc18047296497b12d04
[GucciGerm] <germaine.balmania@gmail.com>
2018-08-22 16:38:10 -0700

    Made getenv change to grab enviromental variable

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify, Flask
+from os import getenv
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
@@ -23,4 +24,5 @@ def page_not_found(exception):
     return (jsonify(d)), 404
 
 if __name__ == "__main__":
-    app.run(threaded=True, host="0.0.0.0", port=5000)
+    app.run(threaded=True, host=getenv("HBNB_API_HOST", default="0.0.0.0"),
+            port=getenv("HBNB_API_PORT", default="5000"))
