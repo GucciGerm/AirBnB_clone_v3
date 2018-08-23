8d613b91e6f814f43ce8f5653e3ed9cb3223ba0e
Sidney Burkett <sidneyriffic@gmail.com>
2018-08-23 09:16:04 -0700

    PEP8 api files

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,25 +3,30 @@
     Create a restful API using flask
 """
 
+
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify, Flask
 from os import getenv
 
+
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views, url_prefix="/api/v1")
+
 
 @app.teardown_appcontext
 def flask_teardown(error):
     """Teardown after flask app runs"""
     storage.close()
 
+
 @app.errorhandler(404)
 def page_not_found(exception):
     """404 page handler"""
     d = {"error": "Not found"}
     return (jsonify(d)), 404
+
 
 if __name__ == "__main__":
     app.run(host=getenv("HBNB_API_HOST", default="0.0.0.0"),
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,11 @@
 """
     Create restful API for hbnb project
 """
+
+
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__)
-from api.v1.views.index import *
-from api.v1.views.states import *
+
+if True:
+    from api.v1.views.index import *
+    from api.v1.views.states import *
